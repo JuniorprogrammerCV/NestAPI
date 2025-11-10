8ad94ad5f76f5883248872f1864dc1928b2bc47f
Junior Delgado <juprogramador@gmail.com>
2025-11-10 14:08:58 +0000

    Update files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,16 @@ import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller()
- export class AuthController {
-   constructor(private authService: AuthService) {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
 
-       @Post('signup')
-    signUp() {
-       return this.authService.signUp();
-    }
+  @Post('signup')
+  signUp() {
+    return this.authService.signUp();
+  }
 
-    @Post('signin')
-    signIn() {
-       return this.authService.signIn();
-    }
-
- }
+  @Post('signin')
+  signIn() {
+    return this.authService.signIn();
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,9 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
-
 @Module({
-    imports: [PrismaModule],
-    controllers: [AuthController],
-    providers: [AuthService],
+  imports: [PrismaModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
